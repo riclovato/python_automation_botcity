a0e3d7481ae4aa58ca7468842c74059e99e828e6
Your Name <you@example.com>
2023-08-21 13:11:50 -0300

    cadastro Produtos

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+
 """
 passos:
  - acessar página web do FlowGPT e inicar chat
@@ -14,7 +15,7 @@ from botcity.core import DesktopBot
 
 user_dir = "C:/Users/riicl/AppData/Roaming/Mozilla/Firefox/Profiles/bacw97z8.default-release"
 entrada = 'Você poderia gerar no formato json com nome "produtos" os dados de 3 produtos eletrôncios ' \
-          'contendo as informações de nome, categoria, código, identificador,preço e quantidade? '
+          'contendo as informações de nome, categoria, codigo, identificador, descricao, preço e quantidade? '
 
 
 def coleta_dados_produtos():
@@ -31,7 +32,7 @@ def coleta_dados_produtos():
     #Acessando o site para iniciar o chat
     bot.browse("https://flowgpt.com/chat")
     bot.maximize_window()
-    bot.wait(2000)
+    bot.wait(10000)
     input_texto = bot.find_element(selector="#scrollableDiv > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(4) > div:nth-child(2) > textarea:nth-child(1)",
                                    by=By.CSS_SELECTOR)
     input_texto.send_keys(entrada)
@@ -62,6 +63,76 @@ def cadastra_produtos(data_frame: pandas.DataFrame):
     bot = DesktopBot()
 
     bot.execute("C:/Fakturama2/Fakturama.exe")
+    
+    if bot.find( "fakturama_logo2", matching=0.97, waiting_time=120000):
+        for index, linha in data_frame.iterrows():
+            nome = linha["nome"]
+            categoria = linha["categoria"]
+            codigo = linha["codigo"]
+            identificador = linha["identificador"]
+            descricao = linha["descricao"]
+            preco = str(linha["preco"]).replace('.',',')
+            preco_custo = str(linha["preco"]  * 0.6).replace('.',',')
+            estoque = linha["quantidade"]
+            
+            if bot.find( "new_product2", matching=0.97, waiting_time=10000):
+                bot.click()
+                bot.wait(1000)
+           
+            if bot.find( "item_number2", matching=0.97, waiting_time=10000): 
+                bot.click_relative(150, 5)
+                bot.kb_type(str(index))
+                bot.tab()
+
+                #Nome
+                bot.paste(nome)
+                bot.tab()
+
+                # Category
+                bot.paste(categoria)
+                bot.tab()
+
+                #GTIN
+                bot.paste(codigo)
+                bot.tab()
+
+                #Supplier code
+                bot.paste(identificador)
+                bot.tab()
+
+                #Description
+                bot.paste(descricao)
+                bot.tab()
+
+                #Preço
+                bot.control_a()
+                bot.paste(preco)
+                bot.tab()
+
+                #Cost Price
+                bot.control_a()
+                bot.paste(preco_custo)
+                bot.tab()
+
+                #Allowance
+                bot.tab()
+
+                #Vat
+                bot.tab()
+
+                #Stock
+                bot.control_a()
+                bot.paste(estoque)
+
+
+                if bot.find( "saveButton2", matching=0.97, waiting_time=10000):
+                    bot.click()
+                    bot.wait(1000)
+                    print('Sucesso!')
+                    bot.control_w()
+        bot.wait(2000)
+        bot.alt_f4()
+        
 
 
 def main():
@@ -71,3 +142,15 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
+
+
+
